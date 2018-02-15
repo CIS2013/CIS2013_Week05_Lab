dce8c4552b6f86f948d235d328f78b199113d366
BobbyRockers <rrockers@gmail.com>
2018-02-14 21:06:44 -0600

    More displays of call by reference insanity

--- call_by_reference.cpp
+++ call_by_reference.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 void add_numbers(int& x, int y){
+	// X is call by reference... the value of first changes each time
 	x+=y;
 }
 
@@ -15,12 +16,14 @@ int main(){
 	cout << "Enter Second number: ";
 	cin >> second;
 	
-	add_numbers(first,second)
+	add_numbers(first,second);
 	cout << "The first time calling add_numbers is: " << first << endl;
 	
-	add_numbers(first,second)
+	add_numbers(second, first);
 	cout << "The second time calling add_numbers is: " << first << endl;
 	
-	add_numbers(first,second)
+	add_numbers(first,second);
 	cout << "The third time calling add_numbers is: " << first << endl;
 }
+
+// CIS2013_Week05_Homework1
